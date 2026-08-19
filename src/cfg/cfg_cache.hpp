ed25801c9ef2bd3f49149a581850725873d63793
Viktor Glebov <electrolviv@gmail.com>
2026-08-19 07:32:58 +0300

    ICACHE / DCACHE Macro

--- src/cfg/cfg_cache.hpp
+++ src/cfg/cfg_cache.hpp
@@ -3,61 +3,66 @@
 // I-CACHE / D-CACHE
 #ifdef VHLIB_OPTIMAL_PLATFORM_EMBEDDED
 
-
-// STM32F407xx
-#if defined(STM32F407xx)
-
-#if defined(VHLIB_OPTIMAL_USE_ICACHE)
-#define VHLIB_OPTIMAL_ICACHE
+#ifdef VHLIB_OPTIMAL_USE_ICACHE
+#define VHLIB_OPTIMAL_FASTFUNC __attribute__((section(VHLIB_OPTIMAL_USE_ICACHE))) 
 #endif
 
-#if defined(VHLIB_OPTIMAL_USE_DCACHE)
-#define VHLIB_OPTIMAL_DCACHE __attribute__((section(".ccmram")))
+#ifdef VHLIB_OPTIMAL_USE_DCACHE 
+#define VHLIB_OPTIMAL_FASTSRAM __attribute__((section(VHLIB_OPTIMAL_USE_DCACHE))) 
 #endif
 
-// STM32F746xx
-#elif defined(STM32F746xx)
+#endif 
 
-#if defined(VHLIB_OPTIMAL_USE_ICACHE)
-#define VHLIB_OPTIMAL_ICACHE __attribute__((section(".itcm_text"), noinline))
-#endif
-
-#if defined(VHLIB_OPTIMAL_USE_DCACHE)
-#define VHLIB_OPTIMAL_DCACHE __attribute__((section(".dtcm_bss")))
-#endif
-
-#define VHLIB_OPTIMAL_FASTFUNC VHLIB_OPTIMAL_ICACHE
-
-// STM32H750xx
-#elif defined(STM32H750xx)
-
-#if defined(VHLIB_OPTIMAL_USE_ICACHE)
-#define VHLIB_OPTIMAL_ICACHE
-#endif
-
-#if defined(VHLIB_OPTIMAL_USE_DCACHE)
-#define VHLIB_OPTIMAL_DCACHE __attribute__((section(".dtcm_bss")))
-#endif
-
-#define VHLIB_OPTIMAL_FASTFUNC VHLIB_OPTIMAL_ICACHE
-
-// Unknown platform
-#else
-
-#endif
-
-
-#endif // VHLIB_OPTIMAL_PLATFORM_EMBEDDED
-
-
-#ifndef VHLIB_OPTIMAL_ICACHE
-#define VHLIB_OPTIMAL_ICACHE
-#endif
-
-#ifndef VHLIB_OPTIMAL_DCACHE
-#define VHLIB_OPTIMAL_DCACHE
-#endif
 
 #ifndef VHLIB_OPTIMAL_FASTFUNC
 #define VHLIB_OPTIMAL_FASTFUNC
 #endif
+
+#ifndef VHLIB_OPTIMAL_FASTSRAM
+#define VHLIB_OPTIMAL_FASTSRAM
+#endif
+
+
+// "noinline" need ?
+
+// // STM32F407xx
+// #if defined(STM32F407xx)
+
+// #if defined(VHLIB_OPTIMAL_USE_ICACHE)
+// #define VHLIB_OPTIMAL_ICACHE
+// #endif
+
+// #if defined(VHLIB_OPTIMAL_USE_DCACHE)
+// #define VHLIB_OPTIMAL_DCACHE __attribute__((section(".ccmram")))
+// #endif
+
+// // STM32F746xx
+// #elif defined(STM32F746xx)
+
+// #if defined(VHLIB_OPTIMAL_USE_ICACHE)
+// #define VHLIB_OPTIMAL_ICACHE __attribute__((section(".itcm_text"), noinline))
+// #endif
+
+// #if defined(VHLIB_OPTIMAL_USE_DCACHE)
+// #define VHLIB_OPTIMAL_DCACHE __attribute__((section(".dtcm_bss")))
+// #endif
+
+// #define VHLIB_OPTIMAL_FASTFUNC VHLIB_OPTIMAL_ICACHE
+
+// // STM32H750xx
+// #elif defined(STM32H750xx)
+
+// #if defined(VHLIB_OPTIMAL_USE_ICACHE)
+// #define VHLIB_OPTIMAL_ICACHE
+// #endif
+
+// #if defined(VHLIB_OPTIMAL_USE_DCACHE)
+// #define VHLIB_OPTIMAL_DCACHE __attribute__((section(".dtcm_bss")))
+// #endif
+
+// #define VHLIB_OPTIMAL_FASTFUNC VHLIB_OPTIMAL_ICACHE
+
+// // Unknown platform
+// #else
+
+// #endif
